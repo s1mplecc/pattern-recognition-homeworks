10aa197e98368d296306c8d9ae85d9fe8b83e528
s1mple <s1mple951205@gmail.com>
2020-11-12 21:10:51 +0800

    complete

--- perceptron.py
+++ perceptron.py
@@ -2,23 +2,51 @@
 
 import numpy as np
 
-a = [[0, 0, 0], [1, 0, 0], [1, 0, 1], [1, 1, 0]]
-b = [[0, 0, 1], [0, 1, 1], [0, 1, 0], [1, 1, 1]]
-w1 = [-1, -2, -2, 0]
 
-
-def standardize(a, b):
+def standardize(_a, _b):
     _np_a = []
     _np_b = []
-    for i in a:
+    for i in _a:
         i.append(1)
         _np_a.append(np.array(i))
-    for i in b:
+    for i in _b:
         i.append(1)
         _np_b.append(np.array(i) * -1)
     return _np_a, _np_b
-    # dot = np.array(a[0]).dot(np.array(w).T)
+
+
+def perceptron(_np_a, _np_b, _w1):
+    print(_np_b)
+    data = _np_a + _np_b
+    print(data)
+    _w = np.array(_w1)
+    _end_flag = False
+    while not _end_flag:
+        _leq_flag = False
+        for vector in data:
+            dot = _w.dot(vector.T)
+            if dot > 0:
+                continue
+            else:
+                _w = _w + vector
+                print(_w)
+                _leq_flag = True
+                continue
+        if _leq_flag is True:
+            _end_flag = False
+            continue
+        else:
+            _end_flag = True
+            print('end')
+            break
+    return _w
 
 
 if __name__ == '__main__':
+    a = [[0, 0, 0], [1, 0, 0], [1, 0, 1], [1, 1, 0]]
+    b = [[0, 0, 1], [0, 1, 1], [0, 1, 0], [1, 1, 1]]
+    w1 = [-1, -2, -2, 0]
+
     np_a, np_b = standardize(a, b)
+    result = perceptron(np_a, np_b, w1)
+    print(result)
